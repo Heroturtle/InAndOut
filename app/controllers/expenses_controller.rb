dce148b2d55800aa99f5e9c2d2eed29e28df54ac
Ralph Haefliger <super@heroturtle.com>
2014-08-11 20:53:41 +0200

    Use Twitter Bootstrap

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,14 +1,11 @@
 class ExpensesController < ApplicationController
 
-  def index
-    @expenses = Expense.order(updated_at: :desc).limit(25)
-  end
+def show
+  @expense = Expense.find(params[:id])
+end
 
-  def show
-    @expense = Expense.find(params[:id])
-  end
 
-  def new
+def new
     @expense = Expense.new
   end
 
@@ -17,12 +14,34 @@ class ExpensesController < ApplicationController
     if @expense.save
       redirect_to @expense, notice: 'Ausgabe wurde gebucht'
     else
-      flash[:alert] = "Etwas ging falsch, genau spezifieren was"
-      render :new
+      render :new, alert: "Etwas ging falsch, genau spezifieren was"
     end
   end
 
+  def edit
+    load_expense
+  end
+
+  def update
+    load_expense
+    if @expense.update(expense_params)
+      redirect_to @expense, notice: 'Ausgabe wurde aktualisiert'
+    else
+      render :edit, alert: "Etwas ging falsch, genau spezifieren was"
+    end
+  end
+
+  def destroy
+    load_expense
+    @expense.destroy
+    redirect_to expenses_path, notice: 'Ausgabe wurde gelöscht'
+  end
+
   private
+
+  def load_expense
+    @expense = Expense.find(params[:id])
+  end
 
   def expense_params
     params.require(:expense).permit(:invoice_number, :amount, :currency_code, :transaction_date, :payment_date, :description)
